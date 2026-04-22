62959db385538d7d93ac38b6f92936997f9687f8
Fatih Ali Ardıç <faliardic@gmail.com>
2026-04-22 17:51:05 +0300

    ana menüye kullanıcı seçimi eklendi

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 def ana_menu():
-    print("PYTHON PROJE")
+    print("\nPYTHON PROJE")
+    print("1- Hesap Makinesi")
+    print("0- Çıkış")
+
+    secim = input("Seçiminiz: ")
+
+    if secim == "1":
+        print("Hesap makinesi seçildi")
+    elif secim == "0":
+        return
 
 ana_menu()
