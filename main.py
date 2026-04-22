64567b057d7002e14f503949eecdaa09cfe984d1
Fatih Ali Ardıç <faliardic@gmail.com>
2026-04-22 18:24:45 +0300

    hesap makinesi menüsüne kullanıcı seçimi eklendi

--- main.py
+++ main.py
@@ -4,6 +4,15 @@ def hesap_makinesi():
     print("2- Çıkarma")
     print("0- Ana Menü")
 
+    secim = input("Seçiminiz: ")
+
+    if secim == "1":
+        print("Toplama seçildi")
+    elif secim == "2":
+        print("Çıkarma seçildi")
+    elif secim == "0":
+        return
+    
 def ana_menu():
     print("\nPYTHON PROJE")
     print("1- Hesap Makinesi")
