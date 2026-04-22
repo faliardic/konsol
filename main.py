943ea8cf30eb2dc568cb786d6f15fd9c41386f56
Fatih Ali Ardıç <faliardic@gmail.com>
2026-04-22 18:23:10 +0300

    hesap makinesi menüsü eklendi

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+def hesap_makinesi():
+    print("\nHESAP MAKİNESİ")
+    print("1- Toplama")
+    print("2- Çıkarma")
+    print("0- Ana Menü")
+
 def ana_menu():
     print("\nPYTHON PROJE")
     print("1- Hesap Makinesi")
@@ -6,7 +12,7 @@ def ana_menu():
     secim = input("Seçiminiz: ")
 
     if secim == "1":
-        print("Hesap makinesi seçildi")
+        hesap_makinesi()
     elif secim == "0":
         return
 
